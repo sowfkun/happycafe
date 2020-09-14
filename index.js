30581f829131c9c3809d730da1f97265458ff3e3
sowfkun <sowfkun@users.noreply.github.com>
2020-09-14 16:31:43 +0700

    Set up circle ci

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.use("", (req,res) => {
 
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+
+var server = app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+module.exports = server;
---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+const supertest = require("supertest");
+const app = require("../index");
+
+describe("Home", () => {
+  after((done) => {
+    app.close(done);
+  });
+
+  it("return status code 200", (done) => {
+    supertest(app)
+      .get("")
+      .expect(200, done);
+  });
+});
